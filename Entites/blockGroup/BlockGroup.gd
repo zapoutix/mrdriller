f2b58f7743ee14aa7a7d6fefc945ae4d25bf800e
zapoutix <zapoutix@gmail.com>
2024-09-08 13:44:31 +0200

    .

--- Entites/blockGroup/BlockGroup.gd
+++ Entites/blockGroup/BlockGroup.gd
@@ -12,8 +12,17 @@ class BlockInfo:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	addBlock(Vector2i(0, 0))
-	addBlock(Vector2i(1, 1))
+	add_block(Vector2i(0, 3))
+	
+	add_block(Vector2i(1, 3))
+	add_block(Vector2i(2, 3))	
+	add_block(Vector2i(3, 3))	
+	add_block(Vector2i(4, 3))	
+	add_block(Vector2i(1, 2))
+	add_block(Vector2i(2, 2))	
+	add_block(Vector2i(3, 2))	
+	add_block(Vector2i(2, 1))	
+	
 	pass # Replace with function body.
 
 
@@ -21,7 +30,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func addBlock(pos: Vector2i) -> void:
+func add_block(pos: Vector2i) -> void:
 	if (_blocks.any(func(i:BlockInfo): return i.position == pos)) :
 		return
 		
@@ -33,7 +42,31 @@ func addBlock(pos: Vector2i) -> void:
 	ins.block = block
 	_blocks.push_back(ins)
 	add_child(block)
+	self.shape_blocks()
 	
 func shape_blocks():
+	for block in _blocks:
+		block.block.reset()
+		if (!_check_close_block_exists(block, Vector2i(-1, 0))):
+			block.block.add_border(MrDrillerBlock.BorderPosition.LEFT)
+		if (!_check_close_block_exists(block, Vector2i(1, 0))):
+			block.block.add_border(MrDrillerBlock.BorderPosition.RIGHT)
+		if (!_check_close_block_exists(block, Vector2i(0, -1))):
+			block.block.add_border(MrDrillerBlock.BorderPosition.TOP)
+		if (!_check_close_block_exists(block, Vector2i(0, 1))):
+			block.block.add_border(MrDrillerBlock.BorderPosition.BOTTOM)
+		
 	pass
 	
+func _get_blockInfo(pos:Vector2i) -> BlockInfo:
+	var found = _blocks.filter(func(i:BlockInfo): return i.position == pos)
+	if (!found.is_empty()):
+		return found[0]
+	
+	return null
+	
+func _check_close_block_exists(from: BlockInfo, direction: Vector2i) -> bool:
+	var pos = from.position
+	var check_pos = pos + direction
+	var close_blockInfo = self._get_blockInfo(check_pos)
+	return close_blockInfo != null
